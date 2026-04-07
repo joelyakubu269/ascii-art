2c7a6f13ec9167e5f7bbc154c9e879497be37f0f
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-07 22:21:02 +0100

    print their ascii values

--- ex2.go
+++ ex2.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	str := "Hello"
 	for _, r := range str {
-		fmt.Println(string(r)+"->", int(r))
+		index := int(r) - 32
+		fmt.Println(string(r), "->", int(r), "index:", index)
 	}
 }
