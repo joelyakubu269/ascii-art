630e2c58d56e4c32b3bca3b7ddefda43e6d55629
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-07 23:54:33 +0100

    printed my first character

--- read.go
+++ read.go
@@ -13,11 +13,17 @@ func main() {
 		log.Fatal(err)
 	}
 	lines := strings.Split(string(data), "\n")
+	s := "HELLO"
+	printChar(s, lines)
 
-	index := 'E' - 32
-
-	start := int(index * 9)
-	for i := 0; i < 8; i++ {
-		fmt.Println(lines[start+i])
+}
+func printChar(s string, lines []string) {
+	for _, r := range s {
+		index := int(r) - 32
+		start := index * 9
+		for i := 0; i < 8; i++ {
+			fmt.Print(lines[start+i])
+		}
+		fmt.Println()
 	}
 }
