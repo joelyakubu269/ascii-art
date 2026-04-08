5e690d93195e6ad5b09e35a9fbab50dc97a18197
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-08 16:01:43 +0100

    starting some exercises

--- ex1.go
+++ ex1.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-
-}
-func printC(s string) {
 	data, err := os.ReadFile("standard.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	lines := strings.Split(string(data), "\n")
-	for _, r := range s {
-		index := int(r) - 32
-		start := index * 9
+	printC("A", lines)
 
+}
+func printC(s string, lines []string) {
+	if s == "" {
+		return
 	}
+
+	for i := 0; i < 8; i++ {
+		for _, r := range s {
+			if r < 32 || r > 126 {
+				continue
+			}
+			index := int(r) - 32
+			start := index * 9
+			fmt.Print(lines[start+i])
+
+		}
+		fmt.Println()
+	}
+
 }
