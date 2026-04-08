3c58185fa81d4824f8bb993cc963dc6628fb4b40
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-08 18:52:00 +0100

    just printing hello vertically

--- read.go
+++ read.go
@@ -8,35 +8,42 @@ import (
 )
 
 func main() {
+	var arg = os.Args[1:]
+	arg1 := strings.Join(arg, " ")
+	// flag.StringVar(&arg, "val", "", "put the text you want to use")
+	// flag.Parse()
 	data, err := os.ReadFile("standard.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	lines := strings.Split(string(data), "\n")
-	s := os.Args[1]
-
-	printChar(s, lines)
+	printC(arg1, lines)
 
 }
-func printChar(s string, lines []string) {
-	newS := strings.Split(s, "\n")
+func printC(s string, lines []string) {
+	if s == "" {
+		return
+	}
+	parts := strings.ReplaceAll(s, `\n`, "\n")
+	parts1 := strings.Split(parts, "\n")
 
-	for _, r := range newS {
+	for _, r := range parts1 {
 		for i := 0; i < 8; i++ {
-
 			for _, c := range r {
-				if c == ' ' {
-					fmt.Print("        ")
+				if c < 32 || c > 126 {
 					continue
 				}
 				index := int(c) - 32
 				start := index * 9
-				fmt.Print(lines[start+i])
+				if start+i < len(lines) {
+					fmt.Print(lines[start+i])
+
+				}
+
 			}
 			fmt.Println()
 
 		}
-
 	}
 
 }
