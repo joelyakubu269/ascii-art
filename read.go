a5e4fde128369f2d05018b631898b708170e7607
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-08 14:25:43 +0100

    printed my first character

--- read.go
+++ read.go
@@ -13,18 +13,29 @@ func main() {
 		log.Fatal(err)
 	}
 	lines := strings.Split(string(data), "\n")
-	s := "HELLO"
+	s := "THere for\nyou"
 	printChar(s, lines)
 
 }
 func printChar(s string, lines []string) {
-	for i := 0; i < 8; i++ {
+	newS := strings.Split(s, "\n")
 
-		for _, r := range s {
-			index := int(r) - 32
-			start := index * 9
-			fmt.Print(lines[start+i])
+	for _, r := range newS {
+		for i := 0; i < 8; i++ {
+
+			for _, c := range r {
+				if c == ' ' {
+					fmt.Print("        ")
+					continue
+				}
+				index := int(c) - 32
+				start := index * 9
+				fmt.Print(lines[start+i])
+			}
+			fmt.Println()
+
 		}
-		fmt.Println()
+
 	}
+
 }
